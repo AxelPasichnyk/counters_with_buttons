2e6e8ab1d2f16f00249b369e36b21969ee70d6be
OleksiiPasichnyk <axelpasechnik@gmail.com>
2020-07-27 23:18:12 -0400

    Updates

---
+++ precache-manifest.ade722e33b602640f292a6c9de2994df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dce13bedc260f84c3981329d1c30eb4",
+    "url": "/counters_with_buttons/index.html"
+  },
+  {
+    "revision": "34c71916ae1691d0808e",
+    "url": "/counters_with_buttons/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1a1d8a0607a5353d88d5",
+    "url": "/counters_with_buttons/static/js/2.c6b55a15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counters_with_buttons/static/js/2.c6b55a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c71916ae1691d0808e",
+    "url": "/counters_with_buttons/static/js/main.2cee90ed.chunk.js"
+  },
+  {
+    "revision": "55f1e4fbd77698f5e0c8",
+    "url": "/counters_with_buttons/static/js/runtime-main.bbffe0b4.js"
+  }
+]);
